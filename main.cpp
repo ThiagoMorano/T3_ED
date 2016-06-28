8f1b48ec9e7b6523073a78228ed1edb4a94909cd
gucolombini <gustavogiordanocolombini@gmail.com>
2016-06-28 03:14:51 -0300

    Changes in main

--- main.cpp
+++ main.cpp
@@ -7,42 +7,22 @@
 #include <Windows.h>
 #include <string>
 #include <sstream>
-#define DIFICULDADE 10
+#define DIFICULDADE 25
 
 sf::Font font;
 
 int main() {
 	sf::RenderWindow* window = new sf::RenderWindow(sf::VideoMode(800, 600), "Mad Genius");
-	//setting text and font
 	sf::Text text, text2, text3, text4, text5, text6, text7;
 	font.loadFromFile("ALGER.ttf");
 	text.setFont(font);
 	text2.setFont(font);
-	/*text3.setFont(font); text3.setColor(sf::Color::Black); text3.setString("1"); text3.setPosition(230, 320);
-	text4.setFont(font); text4.setColor(sf::Color::Black); text4.setString("3"); text4.setPosition(430, 320);
-	text5.setFont(font); text5.setColor(sf::Color::Black); text5.setString("2"); text5.setPosition(330, 320);
-	text6.setFont(font); text6.setColor(sf::Color::Black); text6.setString("4"); text6.setPosition(530, 320);*/
 	text7.setFont(font); text7.setColor(sf::Color::Black); text7.setString("Mad-Genius"); text7.setPosition(400, 300);
 	text2.setPosition(600, 600);
 	text2.setString("Level");
-	//setting circle settings
-	/*sf::CircleShape greenCircle(40.0f);
-	sf::CircleShape magentaCircle(40.0f);
-	sf::CircleShape redCircle(40.0f);
-	sf::CircleShape blueCircle(40.0f);
-	greenCircle.setFillColor(sf::Color::Green);
-	magentaCircle.setFillColor(sf::Color::Magenta);
-	redCircle.setFillColor(sf::Color::Red);
-	blueCircle.setFillColor(sf::Color::Blue);
-	greenCircle.setPosition(200, 300);
-	magentaCircle.setPosition(400, 300);
-	redCircle.setPosition(300, 300);
-	blueCircle.setPosition(500, 300);*/
-	//green, red, magenta, blue
-	//game functioning variables
 	bool rodando = true;
-	srand(time(NULL));
-	int corGenius;
+	srand(time(0));
+	int corGenius, corGeniusAux = 0;
 	int AuxInsere = 5;
 	int turno = 1, nivel = 1;
 	int i;
@@ -56,13 +36,10 @@ int main() {
 	geniusRender* gR = new geniusRender(window);
 
 	while (window->isOpen()) {
-		/* creating the event */
-
 		sf::Event event;
 		while (window->pollEvent(event)) {
 			if (event.type == sf::Event::Closed)
 				window->close();
-			/*probably the game is going on under here*/
 			i = 0;
 			while (rodando && (turno < DIFICULDADE)) {
 				window->clear(sf::Color::Black);
@@ -70,6 +47,12 @@ int main() {
 
 				while (i < turno) {
 					corGenius = rand() % 4 + 1;
+					while (corGenius == corGeniusAux) {
+						corGenius = rand() % 4 + 1;
+					}
+					corGeniusAux = corGenius;
+
+					printf("%d",corGenius);
 					p1.Empilha(corGenius, rodando);
 
 					if (turno > 1) {
@@ -84,22 +67,12 @@ int main() {
 							p1.Empilha(auxDesempilha, rodando);
 						}
 					}
-					/* we'll set the circles with white, showing that they are part of the sequence
-					and the Sleep() function was added after every p1.Empilha() to TRY(?) to make it slow-mode */
 					gR->blinkCircle(corGenius);
 					Sleep(1000);
 					i++;
 				}
 
 				for (int i = 0; i < turno && turno < DIFICULDADE; i++) {
-
-					/* im not printing the "pilha" as we did with numbers before
-					and the AuxInsere variable was created to compare with the corGenius
-
-					we'll determine the AuxInsere variable according to the keypressed variable
-					as it should be A, S, D or F
-
-					the same approach with the white values was used */
 					while (AuxInsere == 5) {
 						if (sf::Keyboard::isKeyPressed(sf::Keyboard::Num1)) {
 							gR->blinkGreenCircle();
@@ -122,17 +95,7 @@ int main() {
 					f1.Insere(AuxInsere, rodando);
 					AuxInsere = 5;
 				}
-				if (turno < DIFICULDADE) {
-					/* Trying to clear the window and show a text */
-					/*window->clear(sf::Color::Black);
-					text.setString("Checking Answer!");
-					window->draw(text);
-					window->display();
-					Sleep(500);
-
-					/* Clearing it again, to show if it was right or wrong
-					that's why the Sleep() was used again: trying to not show the messages too fast */
-
+				if (turno < DIFICULDADE-14) {
 					window->clear(sf::Color::Black);
 
 					if (p1.PilhasIguais(f1)) {
@@ -144,7 +107,7 @@ int main() {
 						window->draw(text);
 						window->draw(text2);
 						window->display();
-						Sleep(2000);
+						Sleep(1000);
 						turno++;
 						while (!f1.Vazia()) {
 							f1.Retira(corGenius, rodando);
@@ -154,6 +117,7 @@ int main() {
 					else {
 						window->clear();
 						text.setString("Wrong Answer, game over!");
+						text.setPosition(200, 200);
 						window->draw(text);
 						window->display();
 						Sleep(1000);
@@ -161,7 +125,8 @@ int main() {
 						/*Sleep used to show another message. Decide if it continues or not */
 
 						window->clear(sf::Color::Black);
-						text.setString("Wanna play again? hit 'Y' if yes, and 'N' if don't");
+						text.setString("Again? 'Y' for yes, 'N' for no");
+						text.setPosition(200, 200);
 						window->draw(text);
 						window->display();
 						Sleep(1000);
