cea2e3bb8d3b730ee60b22c321655f58c5bcc762
gucolombini <gustavogiordanocolombini@gmail.com>
2016-06-28 21:02:06 -0300

    Added new files and commands text

--- geniusRender.h
+++ geniusRender.h
@@ -1,14 +1,17 @@
 #include <SFML\Graphics.hpp>
+#include <SFML\Audio.hpp>
 #include <Windows.h>
 
 #ifndef GENIUSRENDER_H
 #define GENIUSRENDER_H
 
 extern sf::Font font;
+//extern sf::SoundBuffer buffer;
 
 class geniusRender {
 
 private:
+	/*sf::Music* music;*/
 	sf::CircleShape* greenCircle;
 	sf::CircleShape* magentaCircle;
 	sf::CircleShape* redCircle;
@@ -16,9 +19,14 @@ private:
 	sf::RenderWindow* window;
 	sf::Text text3, text4, text5, text6, text7;
 	sf::Text text8;
+	sf::Text text9;
 
 public:
 	geniusRender(sf::RenderWindow* w) {
+		/*music = new sf::Music;
+		music->openFromFile("switch.wav");*/
+
+
 		greenCircle = new sf::CircleShape(40.0f);
  		magentaCircle = new sf::CircleShape(40.0f);
 		redCircle = new sf::CircleShape(40.0f);
@@ -33,7 +41,8 @@ public:
 		magentaCircle->setPosition(400, 300);
 		redCircle->setPosition(300, 300);
 		blueCircle->setPosition(500, 300);
-
+		
+		text9.setFont(font); text9.setColor(sf::Color::Yellow); text9.setString("Comands: Keyboard 1 - 2 - 3 - 4"); text9.setPosition(160, 500);
 		text3.setFont(font); text3.setColor(sf::Color::Black); text3.setString("1"); text3.setPosition(230, 320);
 		text4.setFont(font); text4.setColor(sf::Color::Black); text4.setString("3"); text4.setPosition(430, 320);
 		text5.setFont(font); text5.setColor(sf::Color::Black); text5.setString("2"); text5.setPosition(330, 320);
@@ -42,24 +51,28 @@ public:
 		text7.setCharacterSize(50);
 		text8.setFont(font); text8.setColor(sf::Color::Yellow); text8.setString("Sequencia Inversa"); text8.setPosition(240, 160);
 	}
-	void draw2() {
+	/*void draw2() {
 		window->draw(text8);
 		window->display();
 	}
+
+	/*void playMusic() {
+		music->play();
+	}*/
 
 	void draw() {
 		window->draw(*greenCircle);
 		window->draw(*magentaCircle);
 		window->draw(*redCircle);
 		window->draw(*blueCircle);
-		window->draw(text3); window->draw(text4); window->draw(text5); window->draw(text6); window->draw(text7); window->draw(text8);
+		window->draw(text3); window->draw(text4); window->draw(text5); window->draw(text6); window->draw(text7); window->draw(text8); window->draw(text9);
 		window->display();
 	}
 
 	void blinkCircle(int cor) {
 		switch (cor) {
 		case 1:
-			//Sleep(100);
+			//Sleep(100);			
 			greenCircle->setFillColor(sf::Color::White);
 			this->draw();
 			Sleep(500);
